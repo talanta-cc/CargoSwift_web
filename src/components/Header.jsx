b78ec7bda3983936815f075995f2cfb2c42dc573
edgarngeleso <edgarngereso@gmail.com>
2024-09-20 09:11:07 +0300

    Updated Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,22 +6,27 @@ function Header() {
     <>
       <div className="contact-wrapper">
         <div className="logo">
-          <i className="fa-solid fa-thin fa-cubes"></i>
+          <img className="fa-solid fa-thin fa-cubes" src='logo.png' style={{
+            width:50,
+            height:50,
+            margin:5,
+            borderRadius:5
+          }}/>
           <p>CargoSwift</p>
         </div>
         <div className="contact">
           <div className="info opening-hours">
             <div className="icon-wrapper"><i className="fa-regular fa-clock"></i></div>
             <div className="info-details">
-              <p>Mon-Sat 9:00-18:00</p>
-              <p>Sunday Closed</p>
+              <p>Open</p>
+              <p>24/7</p>
             </div>
           </div>
           <div className="email info">
             <div className="icon-wrapper"><i className="fa-regular fa-envelope"></i></div>
             <div className="info-details">
               <p>Email</p>
-              <p>contact@transitflow.com</p>
+              <p>contact@cargoswift.com</p>
             </div>
           </div>
           <div className="phone info">
